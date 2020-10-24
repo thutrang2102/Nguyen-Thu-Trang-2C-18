2db7143510927cfd6c308107735fe24db52a48c5
thutrang2102 <71474020+thutrang2102@users.noreply.github.com>
2020-10-24 23:47:09 +0700

    Add files via upload

---
+++ HW/Week6/pet_class.py
@@ -0,0 +1,25 @@
+class pet:
+    def __init__(self, name, animal_type, age):
+        self.name = name
+        self.animal_type = animal_type
+        self.age = age
+
+    def set_name(self):
+            self.name
+    def get_name(self):
+            return self.name
+
+    def set_animal_type(self):
+            self.animal_type
+    def get_animal_type(self):
+            return self.animal_type
+
+    def set_age(self):
+            self.age
+    def get_age(self):
+            return self.age
+    def printResult(self):
+        print( self.name, self.animal_type, self.age)
+if __name__=="__main__":
+    A = pet("dog", "poodle", 1)
+    A.printResult()
